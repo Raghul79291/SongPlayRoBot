8ec18915ac01dc905655592c9ad17d24af1653a0
Raghul79291 <94465206+Raghul79291@users.noreply.github.com>
2023-04-25 10:37:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("22058221"))
+API_HASH = os.getenv("7d2453623cc2b68f3560edfb610e3dc0")
+BOT_TOKEN = os.getenv("5667003589:AAHahG1prVECnazZIGRBNsS-TB3CkgLzun8")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+OWNER_ID = list({int(x) for x in os.environ.get("@raghul_0ott", "").split()})
